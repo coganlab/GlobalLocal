9fd37edc73e9b855eec4a4a98c4e92e00f48b86f
Jim <jzhang7345@gmail.com>
2023-07-20 14:45:23 -0400

    finally got RTs working thank god

---
+++ misc_functions.py
@@ -0,0 +1,18 @@
+import mne
+
+def calculate_reaction_times(raw):
+    annotations = raw.annotations
+    reaction_times = []
+    skipped = []
+
+    for i in range(len(annotations) - 1):
+        current_annotation = annotations[i]
+        next_annotation = annotations[i + 1]
+        if 'Stimulus' in current_annotation['description']:
+            if 'Response' in next_annotation['description']:
+                reaction_time = next_annotation['onset'] - current_annotation['onset']
+                reaction_times.append(reaction_time)
+            else:
+                skipped.append(i)
+
+    return reaction_times, skipped
