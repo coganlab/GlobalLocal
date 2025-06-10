e4b530e8261e08a66bd37b66c572b0f1d46548ad
Erin <etb28@duke.edu>
2025-06-10 14:15:49 -0400

    count trials scripts made

---
+++ dcc_scripts/spec/sbatch_count_trials.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --output=out/aligned_svm_ncv/slurm_%j.out
+#SBATCH -e out/aligned_svm_ncv/slurm_%j.err
+#SBATCH -p common,scavenger
+#SBATCH -c 5
+#SBATCH --mem=20G
+
+subject=$1
+
+source $(conda info --base)/etc/profile.d/conda.sh
+
+conda activate ieeg # make sure this works
+
+#to be able to search for src file
+export PYTHONPATH=/hpc/home/$USER/coganlab/$USER/GlobalLocal
+
+python /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/spec/count_trials_dcc.py --subject ${subject}
