760353e73b45c93e0b170736fb4b4551bbc6e71f
Erin <etb28@duke.edu>
2025-06-20 15:43:28 -0400

    ran wavelet diff on DCC

--- dcc_scripts/spec/submit_wavelet_differences.sh
+++ dcc_scripts/spec/submit_wavelet_differences.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# loop over subjects and submit the sbatch script for plot clean
-#subjects=("D0057" "D0059" "D0063" "D0065" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110")
-
-subjects=('D0094')
+# loop over subjects except D0094 cause already done
+subjects=("D0057" "D0059" "D0063" "D0065" "D0069" "D0071" "D0077" "D0090" "D0100" "D0102" "D0103" "D0107A" "D0110")
 
 for subject in "${subjects[@]}"
 do
--- dcc_scripts/spec/wavelet_differences_dcc.py
+++ dcc_scripts/spec/wavelet_differences_dcc.py
@@ -59,8 +59,8 @@ else: # mac
 
 layout = get_data('GlobalLocal', root=LAB_root)
 
-# subjects = ['D0057', 'D0059', 'D0063', 'D0065', 'D0069', 'D0071', 'D0077', 'D0090', 'D0094', 'D0100', 'D0102', 'D0103', 'D0107A', 'D0110']
-subjects = ['D0094']
+subjects = ['D0057', 'D0059', 'D0063', 'D0065', 'D0069', 'D0071', 'D0077', 'D0090', 'D0100', 'D0102', 'D0103', 'D0107A', 'D0110']
+#subjects = ['D0094']
 
 # this is a toggle for which version to run - the one that makes the wavelets in this notebook directly, or the one that loads them
 make_wavelets=False
