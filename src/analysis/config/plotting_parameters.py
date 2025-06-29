dcc46010b092545dff2663ce6e471eaf967ed228
jimzhang629 <46616647+jimzhang629@users.noreply.github.com>
2025-06-29 11:54:41 -0400

    made some plotting code for evoked objects, go through this later today to clean it up and actually understand it

---
+++ src/analysis/config/plotting_parameters.py
@@ -0,0 +1,112 @@
+plotting_parameters = {
+    'Stimulus_r': {
+    'condition_parameter': 'repeat',
+    'color': 'blue',
+    "line_style": "-"
+},
+'Stimulus_s': {
+    'condition_parameter': 'switch',
+    'color': 'blue',
+    "line_style": "--"
+},
+'Stimulus_c': {
+    'condition_parameter': 'congruent',
+    'color': 'red',
+    "line_style": "-"
+},
+'Stimulus_i': {
+    'condition_parameter': 'incongruent',
+    'color': 'red',
+    "line_style": "--"
+},
+"Stimulus_ir": {
+    "condition_parameter": "IR",
+    "color": "blue",
+    "line_style": "-"
+},
+"Stimulus_is": {
+    "condition_parameter": "IS",
+    "color": "blue",
+    "line_style": "--"
+},
+"Stimulus_cr": {
+    "condition_parameter": "CR",
+    "color": "red",
+    "line_style": "-"
+},
+"Stimulus_cs": {
+    "condition_parameter": "CS",
+    "color": "red",
+    "line_style": "--"
+},
+"Stimulus_c25": {
+    "condition_parameter": "c75",
+    "color": "pink",
+    "line_style": "-"
+},
+"Stimulus_c75": {
+    "condition_parameter": "c25",
+    "color": "orange",
+    "line_style": "-"
+},
+"Stimulus_i25": {
+    "condition_parameter": "i75",
+    "color": "pink",
+    "line_style": "--"
+},
+"Stimulus_i75": {
+    "condition_parameter": "i25",
+    "color": "orange",
+    "line_style": "--"
+},
+"Stimulus_s25": {
+    "condition_parameter": "s25",
+    "color": "skyblue",
+    "line_style": "--"
+},
+"Stimulus_s75": {
+    "condition_parameter": "s75",
+    "color": "purple",
+    "line_style": "--"
+},
+"Stimulus_r25": {
+    "condition_parameter": "r25",
+    "color": "skyblue",
+    "line_style": "-"
+},
+"Stimulus_r75": {
+    "condition_parameter": "r75",
+    "color": "purple",
+    "line_style": "-"
+},
+"Stimulus_bigH": {
+    "condition_parameter": "bigH",
+    "color": "green",
+    "line_style": "-"
+},
+"Stimulus_bigS": {
+    "condition_parameter": "bigS",
+    "color": "green",
+    "line_style": "--"
+},
+"Stimulus_smallH": {
+    "condition_parameter": "smallH",
+    "color": "orange",
+    "line_style": "-"
+},
+"Stimulus_smallS": {
+    "condition_parameter": "smallS",
+    "color": "orange",
+    "line_style": "--"
+},
+"Stimulus_taskG": {
+    "condition_parameter": "taskG",
+    "color": "gray",
+    "line_style": "-"
+},
+"Stimulus_taskL": {
+    "condition_parameter": "taskL",
+    "color": "gray",
+    "line_style": "--"
+}
+}
