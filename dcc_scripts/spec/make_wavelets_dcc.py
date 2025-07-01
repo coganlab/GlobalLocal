b613595f2b684d4b930c92161526f9b18f077747
Erin <etb28@duke.edu>
2025-07-01 16:40:32 -0400

    reverting back to original hard-coded

--- dcc_scripts/spec/make_wavelets_dcc.py
+++ dcc_scripts/spec/make_wavelets_dcc.py
@@ -37,7 +37,7 @@ import numpy as np
 from src.analysis.utils.general_utils import get_good_data
 from src.analysis.spec.wavelet_functions import get_uncorrected_wavelets
 
-def main(subject_id, conditions_to_run):
+def main(subject_id):
     """Main function to run wavelet analysis for a single subject."""
 
     try:
@@ -52,18 +52,9 @@ def main(subject_id, conditions_to_run):
         # Load Data using the first 'get_data' function
         layout = get_data(task, root=LAB_root)
 
-        all_conditions_dict = {
-            'ErrorTrials_Response_Locked': ["Response/Accuracy/0.0"],
-            'CorrectTrials_Response_Locked': ["Response/Accuracy/1.0"],
-            'ErrorTrials_Stimulus_Locked': ["Stimulus/Accuracy/0.0"],
-            'CorrectTrials_Stimulus_Locked': ["Stimulus/Accuracy/1.0"]
-        }
-
-        output_names_and_events_dict = {
-            name : all_conditions_dict[name]
-            for name in conditions_to_run
-            if name in all_conditions_dict
-        }
+        output_names_and_events_dict = {}
+        output_names_and_events_dict['ErrorTrials_Response_locked'] = ["Responded1.0/Response/Accuracy0.0"]
+        output_names_and_events_dict['CorrectTrials_Response_locked'] = ["Responded1.0/Response/Accuracy1.0"]
 
         baseline_times = [-0.5, 0]
         signal_times = [-0.5, 1.5]
@@ -112,6 +103,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Make wavelets for a given subject.")
     parser.add_argument('--subject', type=str, required=True, 
                         help='The subject ID to processs')
-    parser.add_argument('--conditions', nargs='+', required=True, help='A list of condition names to run')
     args = parser.parse_args()
-    main(args.subject, args.conditions)
+    main(args.subject)
