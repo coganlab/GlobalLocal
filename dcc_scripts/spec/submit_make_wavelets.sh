373cf86e5f52c2ffd38a61dba8d1d571bac84edb
Erin <etb28@duke.edu>
2025-07-02 16:38:30 -0400

    changes to plot_subjects_erin

--- dcc_scripts/spec/submit_make_wavelets.sh
+++ dcc_scripts/spec/submit_make_wavelets.sh
@@ -5,9 +5,7 @@
 #subjects=("D0057" "D0059" "D0063" "D0065" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110" "D0117")
 subjects=("D0065")
 
-conditions_to_run=("ErrorTrials_Response_Locked" "CorrectTrials_Response_Locked")
-
 for subject in "${subjects[@]}"
 do
-    sbatch /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/spec/sbatch_make_wavelets.sh $subject "{conditions_to_run[@]}"
+    sbatch /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/spec/sbatch_make_wavelets.sh $subject 
 done
