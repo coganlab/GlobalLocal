a10112fdeb808dd7c6631d6851789c8dfab252ad
Erin <etb28@duke.edu>
2025-07-08 15:23:16 -0400

    setting make_multitaper to False

--- dcc_scripts/spec/submit_make_wavelets.sh
+++ dcc_scripts/spec/submit_make_wavelets.sh
@@ -4,7 +4,8 @@
 #no D0094 cause I already did that one
 #subjects=("D0057" "D0059" "D0063" "D0065" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110" "D0117")
 subjects=("D0063")
-analysis_type = "multitaper"
+
+analysis_type="multitaper"
 
 for subject in "${subjects[@]}"
 do
