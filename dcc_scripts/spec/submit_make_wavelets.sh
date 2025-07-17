c25ac9e70e9c230de97a5fcd23f8ca682384a4c8
Erin <etb28@duke.edu>
2025-07-17 13:25:34 -0400

    running multitaper on D0059

--- dcc_scripts/spec/submit_make_wavelets.sh
+++ dcc_scripts/spec/submit_make_wavelets.sh
@@ -5,7 +5,7 @@
 #subjects=("D0057" "D0059" "D0065" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110" "D0117")
 subjects=("D0059")
 
-analysis_type="wavelet"
+analysis_type="multitaper"
 
 for subject in "${subjects[@]}"
 do
