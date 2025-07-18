83bd268474615ef7a204eb11fa028a680de5ed64
Erin <etb28@duke.edu>
2025-07-17 22:33:03 -0400

    fixing plot wavelets

--- dcc_scripts/spec/inspect_spec.py
+++ dcc_scripts/spec/inspect_spec.py
@@ -2,7 +2,8 @@ import mne
 import numpy as np
 import os
 
-file_path = '/Users/erinburns/Library/CloudStorage/Box-Box/CoganLab/BIDS-1.1_GlobalLocal/BIDS/derivatives/spec/multitaper/D0057/CorrectTrials_Response_Locked_rescaled-tfr.h5'
+#file_path = '/Users/erinburns/Library/CloudStorage/Box-Box/CoganLab/BIDS-1.1_GlobalLocal/BIDS/derivatives/spec/multitaper/D0059/CorrectTrials_Stimulus_Locked_rescaled-tfr.h5'
+file_path = '/cwork/etb28/BIDS-1.1_GlobalLocal/BIDS/derivatives/spec/multitaper/D0059/CorrectTrials_Stimulus_Locked_rescaled-tfr.h5'
 tfr = mne.time_frequency.read_tfrs(file_path)
 freq_values = tfr.freqs
 print(freq_values)
