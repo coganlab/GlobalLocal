025a2f966827b96776058e6b1f55c3534f4ff726
Erin <etb28@duke.edu>
2025-11-04 15:58:27 -0500

    trying to fix my ieeg issues

---
+++ dcc_scripts/power/check_env.py
@@ -0,0 +1,9 @@
+import sys
+import numpy
+import os
+
+print("--- STARTING ENVIRONMENT CHECK ---")
+print(f"Python Executable: {sys.executable}")
+print(f"NumPy Version: {numpy.__version__}")
+print(f"NumPy Path: {numpy.__file__}")
+print("--- END ENVIRONMENT CHECK ---")
--- dcc_scripts/power/sbatch_power_traces_dcc.sh
+++ dcc_scripts/power/sbatch_power_traces_dcc.sh
@@ -6,15 +6,21 @@
 #SBATCH --mem=30G
 #SBATCH --time=120:00:00  # 48 hours (adjust based on your needs)
 
-unset PYTHONPATH
-
 source $(conda info --base)/etc/profile.d/conda.sh
 
 conda activate ieeg # make sure this works
 
-CONDA_ENV_PY="/hpc/home/$USER/miniconda3/envs/ieeg/bin/python"
-SCRIPT_TO_RUN="/hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py"
+# 4. Define the EXACT python executable you want to use
+THE_RIGHT_PYTHON="/hpc/home/etb28/miniconda3/envs/ieeg/bin/python"
 
-$CONDA_ENV_PY $SCRIPT_TO_RUN
+# 5. Define the script to run
+THE_SCRIPT="/hpc/home/etb28/coganlab/etb28/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py"
+#THE_SCRIPT="/hpc/home/etb28/coganlab/etb28/GlobalLocal/dcc_scripts/power/check_env.py"
+
+# 6. Run the script using that specific python in UNBUFFERED mode
+echo "--- sbatch: Running script with: $THE_RIGHT_PYTHON -u ---"
+
+# The -u flag is the important part
+$THE_RIGHT_PYTHON -u $THE_SCRIPT
 
 #python /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py
