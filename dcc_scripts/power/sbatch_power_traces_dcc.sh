5c17e3f50575f60fb38516091c62d75576065327
Erin <etb28@duke.edu>
2025-11-05 11:22:12 -0500

    going to run err-corr decoding

--- dcc_scripts/power/sbatch_power_traces_dcc.sh
+++ dcc_scripts/power/sbatch_power_traces_dcc.sh
@@ -10,17 +10,4 @@ source $(conda info --base)/etc/profile.d/conda.sh
 
 conda activate ieeg # make sure this works
 
-# 4. Define the EXACT python executable you want to use
-THE_RIGHT_PYTHON="/hpc/home/etb28/miniconda3/envs/ieeg/bin/python"
-
-# 5. Define the script to run
-THE_SCRIPT="/hpc/home/etb28/coganlab/etb28/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py"
-#THE_SCRIPT="/hpc/home/etb28/coganlab/etb28/GlobalLocal/dcc_scripts/power/check_env.py"
-
-# 6. Run the script using that specific python in UNBUFFERED mode
-echo "--- sbatch: Running script with: $THE_RIGHT_PYTHON -u ---"
-
-# The -u flag is the important part
-$THE_RIGHT_PYTHON -u $THE_SCRIPT
-
-#python /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py
+python /hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/power/run_power_traces_dcc.py
