8b0433db7bbb44b5b53dd6d7ec98b60bbdd17a1a
jimzhang629 <jzhang7345@gmail.com>
2026-08-25 20:56:31 -0400

    save

--- dcc_scripts/preproc/submit_make_epoched_data.sh
+++ dcc_scripts/preproc/submit_make_epoched_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # loop over subjects and submit the sbatch script for plot clean
-subjects=("D0057" "D0059" "D0065" "D0063" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110" "D0116" "D0117" "D0121" "D0130" "D0133" "D0134")
+# subjects=("D0057" "D0059" "D0065" "D0063" "D0069" "D0071" "D0077" "D0090" "D0094" "D0100" "D0102" "D0103" "D0107A" "D0110" "D0116" "D0117" "D0121" "D0130" "D0133" "D0134")
 # subjects=("D0130" "D0133" "D0134" "D0137")
 # subjects=("D0065")
 # subjects=("D0137" "D0138" "D0139A" "D0144" "D0145" "D0146")
@@ -18,11 +18,11 @@ PAD_LENGTH=3 # this needs to at least be 1s for theta to avoid edge artifacts. C
 LAB_ROOT="/cwork/jz421"
 CHANNELS="None"
 DEC_FACTOR=8
-OUTLIER_POLICY="drop"
+OUTLIER_POLICY="drop_and_nan"
 OUTLIERS=10
 THRESHOLD_PERCENT=5.0
-PASSBAND="4 8"
-FILTER_METHOD="bandpass" # choose either filterbank_hilbert or bandpass for now.
+PASSBAND="70 150"
+FILTER_METHOD="filterbank_hilbert" # choose either filterbank_hilbert or bandpass for now.
 METHOD='fir' # the filter method for bandpass, if you choose to use bandpass. Can be fir or iir. Irrelevant if you use filterbank_hilbert.
 FIR_DESIGN='firwin' # the fir design if you choose to use fir for bandpass. Irrelevant if you use filterbank_hilbert or you use bandpass with an iir filter.
 SBATCH_SCRIPT_PATH="/hpc/home/$USER/coganlab/$USER/GlobalLocal/dcc_scripts/preproc/sbatch_make_epoched_data.sh"
